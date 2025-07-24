a99e200e70907f5a9ca3d0d192c0a3e7d3907059
Ray <rayhidayat220@gmail.com>
2025-07-24 07:55:51 +0700

    init vite and shadcn comp

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <div className="container mx-auto bg-slate-200 min-h-screen p-2">
+      <div className="animate-in fade-in slide-in-from-top-8 duration-500 text-2xl mt-20">
+        Fade in from 0% opacity,
+        <br />
+        slide from top by 8 spacing units (2rem),
+        <br />
+        with a 500ms duration.
+      </div>
+    </div>
+  );
+}
+
+export default App;
